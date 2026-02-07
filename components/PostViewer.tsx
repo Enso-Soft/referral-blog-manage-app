78aa736a4d86e4b1520bc742e6412c56e57a5b1b
kukwonho <kukwonho@gmail.com>
2026-02-07 19:30:28 +0900

    미리보기 css 수정

--- components/PostViewer.tsx
+++ components/PostViewer.tsx
@@ -8,7 +8,7 @@ export function PostViewer({ content }: PostViewerProps) {
   return (
     <div className="bg-white dark:bg-gray-800 rounded-lg border border-gray-200 dark:border-gray-700 p-6">
       <div
-        className="tiptap max-w-none text-gray-900 dark:text-gray-100"
+        className="max-w-none text-gray-900 dark:text-gray-100"
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
